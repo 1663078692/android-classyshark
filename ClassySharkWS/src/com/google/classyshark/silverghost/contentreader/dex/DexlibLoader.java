770de48432b5c68b491549b4eeff0e6432e5a7cd
Boris Farber <boris.farber@gmail.com>
2017-12-28 12:40:19 +0200

    added dexlib2 patch to support dex 38 format

--- ClassySharkWS/src/com/google/classyshark/silverghost/contentreader/dex/DexlibLoader.java
+++ ClassySharkWS/src/com/google/classyshark/silverghost/contentreader/dex/DexlibLoader.java
@@ -9,7 +9,7 @@ import org.jf.dexlib2.iface.DexFile;
 public class DexlibLoader {
     public static DexFile loadDexFile(File binaryArchiveFile) throws Exception {
         DexBackedDexFile newDexFile = DexFileFactory.loadDexFile(binaryArchiveFile,
-                Opcodes.forApi(19));
+                Opcodes.forApi(38));
 
         return newDexFile;
     }
